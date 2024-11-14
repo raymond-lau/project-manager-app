ff4071d45e0321ea02fb5e3e8a59bfb902f17464
raymond-lau <raymond.lau10@hotmail.com>
2024-11-14 19:39:47 +1100

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{response::{Html, IntoResponse}, routing::get, Router};
+
+async fn get_project() -> impl IntoResponse {
+    Html("Project: <strong>world!</strong>")
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route(
+        "/", 
+        get(get_project)
+    );
+
+    Ok(router.into())
+}
